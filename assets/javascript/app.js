d26949045b232cfa10ce8a374c2da63aa8400eb3
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-01-10 16:25:17 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,13 +20,17 @@ var timer = {
 };
 
 // ------ *** Current Game *** ------ //
-var questions = ["question1", "question2", "question3", "question4", "question5"];
-var arrayOfAnswers = ["1", "2", "3", "4", "5"];
+var questions = ["What is the name of Harry Potter's owl?", 
+"What is the street address of the Dursley home?",
+"Which of the Hogwarts founders created the Chamber of Secrets?", 
+"What Animagus form is taken by Sirius Black?", 
+"Which of these spells is used to unlock doors? "];
+var arrayOfAnswers = ["1", "2", "3", "3", "1"];
 
 var currentGame = {
   homePage: function(){
     $("header").html("<h1>Trivia</h1>");
-    $("#game-window").html("Welcome to trivia");
+    $("#game-window").html("Welcome to Trivia");
     $("#game-window").append("<br>Click the start button to play");
     $("#game-window").append("<br>NOTE: <br>You have 30 seconds to answer all the questions");
     $("#game-window").append("<br><button id='start'>Start Game</button>");
@@ -36,32 +40,47 @@ var currentGame = {
     
     $("#game-window").append("<br>" + questions[0] + "<br>");
     $("#game-window").append("<input id='btn0' type='radio' value='1' name='answer0'>");
+    $("#game-window").append("Hedwig   ");
     $("#game-window").append("<input id='btn1' type='radio' value='2' name='answer0'>");
+    $("#game-window").append("Errol   ");
     $("#game-window").append("<input id='btn2' type='radio' value='3' name='answer0'>");
+    $("#game-window").append("Crookshanks   ");
     $("#game-window").append("<br>");
 
     $("#game-window").append("<br>" + questions[1] + "<br>");
     $("#game-window").append("<input id='btn0a' type='radio' value='1' name='answer1'>");
+    $("#game-window").append("Grimmauld Place   ");
     $("#game-window").append("<input id='btn1a' type='radio' value='2' name='answer1'>");
+    $("#game-window").append("Privet Drive   ");
     $("#game-window").append("<input id='btn2a' type='radio' value='3' name='answer1'>");
+    $("#game-window").append("Little Hangleton   ");
     $("#game-window").append("<br>");
 
     $("#game-window").append("<br>" + questions[2] + "<br>");
     $("#game-window").append("<input id='btn0b' type='radio' value='1' name='answer2'>");
+    $("#game-window").append("Neville Longbottom   ");
     $("#game-window").append("<input id='btn1b' type='radio' value='2' name='answer2'>");
+    $("#game-window").append("Rowena Ravenclaw   ");
     $("#game-window").append("<input id='btn2b' type='radio' value='3' name='answer2'>");
+    $("#game-window").append("Salazar Slytherin   ");
     $("#game-window").append("<br>");
 
     $("#game-window").append("<br>" + questions[3] + "<br>");
     $("#game-window").append("<input id='btn0c' type='radio' value='1' name='answer3'>");
+    $("#game-window").append("Werewolf   ");
     $("#game-window").append("<input id='btn1c' type='radio' value='2' name='answer3'>");
+    $("#game-window").append("Stag   ");
     $("#game-window").append("<input id='btn2c' type='radio' value='3' name='answer3'>");
+    $("#game-window").append("Dog   ");
     $("#game-window").append("<br>");
 
     $("#game-window").append("<br>" + questions[4] + "<br>");
     $("#game-window").append("<input id='btn0d' type='radio' value='1' name='answer4'>");
+    $("#game-window").append("Alohomora   ");
     $("#game-window").append("<input id='btn1d' type='radio' value='2' name='answer4'>");
+    $("#game-window").append("Accio   ");
     $("#game-window").append("<input id='btn2d' type='radio' value='3' name='answer4'>");
+    $("#game-window").append("Dissendium   ");
     $("#game-window").append("<br>");
 
     $("#game-window").append("<br><button id='submit'>Submit</button>");
@@ -104,7 +123,7 @@ var currentGame = {
       if (answers[i].checked){
         var val = $("#btn"+i+"a").val();
         console.log(val);
-        var rightAnswer = arrayOfAnswers[0];
+        var rightAnswer = arrayOfAnswers[1];
 
         if (val == rightAnswer){
           correct++;
@@ -130,7 +149,7 @@ var currentGame = {
       if (answers[i].checked){
         var val = $("#btn"+i+"b").val();
         console.log(val);
-        var rightAnswer = arrayOfAnswers[0];
+        var rightAnswer = arrayOfAnswers[2];
 
         if (val == rightAnswer){
           correct++;
@@ -156,7 +175,7 @@ var currentGame = {
       if (answers[i].checked){
         var val = $("#btn"+i+"c").val();
         console.log(val);
-        var rightAnswer = arrayOfAnswers[0];
+        var rightAnswer = arrayOfAnswers[2];
 
         if (val == rightAnswer){
           correct++;
