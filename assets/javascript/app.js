c4dfba8d50b4e41ab76e12c1fc9dfd74a01df1d3
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-01-10 13:46:22 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,3 @@
-window.onload = function() {
-  currentGame.homePage();
-  
-  $("#start").on("click", function(){
-    timer.start();
-    currentGame.newGame();    
-  });
-
-  $("#submit").click(currentGame.endGame);
-  };
-
 // ------ *** Timer *** ------ //
 var interval;
 
@@ -27,13 +16,12 @@ var timer = {
       currentGame.endGame();
     }
 
-    console.log(timer.time);
   }
 };
 
 // ------ *** Current Game *** ------ //
 var questions = ["question1", "question2", "question3"];
-var answers = ["answer1", "answer2", "answer3"];
+// var answers = ["answer1", "answer2", "answer3"];
 
 var currentGame = {
   homePage: function(){
@@ -48,18 +36,58 @@ var currentGame = {
     for(var i = 0; i < questions.length; i++){
       $("#game-window").append("<br>" + questions[i]);
 
-      $("#game-window").append("<br><input type='radio' name='answer" +i+ "'>");
-      $("#game-window").append("<input type='radio' name='answer" +i+ "'>");
-      $("#game-window").append("<input type='radio' name='answer" +i+ "'>");
+      $("#game-window").append("<br><input type='radio' value='1' name='answer" +i+ "'>");
+      $("#game-window").append("<input type='radio' value='2' name='answer" +i+ "'>");
+      $("#game-window").append("<input type='radio' value='3' name='answer" +i+ "'>");
     }
 
     $("#game-window").append("<br><button id='submit'>Submit</button>");
 
   },
   endGame: function(){
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
+
+
+    var answers = document.getElementsByName("answer0");
+    var state = $("input[name='answer0']").val();
+    console.log(state);
+
+    for (var i = 0; i < answers.length; i++){
+      if (answers[i].checked){
+        if (answers[i].value === 1){
+          correct++;
+        }
+        else{
+          incorrect++;
+        }
+      }
+      else{
+        unanswered++;
+      }
+    }
+
+    $("#game-window").html("Answered Correctly: " + correct);
+    $("#game-window").append("<br>Answered Incorrectly: " + incorrect);
+    $("#game-window").append("<br>Unanswered: " + unanswered);
   },
 };
+
+window.onload = function() {
+  currentGame.homePage();
+  
+  $("#start").on("click", function(){
+    timer.start();
+    currentGame.newGame();    
+  });
+
+  $(document).on("click", "#submit", function(){
+    clearInterval(interval);
+    currentGame.endGame();
+  });
+}
 
 
 
